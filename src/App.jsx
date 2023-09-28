5d0c9a74396f827e43631ecd3c83144c949798fe
Aravinda krishnan <akrishnanh@presidio.com>
2023-09-28 15:40:55 +0530

    Added nextui library

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@nextui-org/react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h3>Hi There 😃! Aspir Ally. Ready...</h3>
+      <Button color="primary">
+        Aspir Ally
+      </Button>
     </div>
   )
 }
